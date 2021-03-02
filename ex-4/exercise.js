e4d38eaf1b5fca310c7ea08e042f01491de11725
fithawi ghide <lgfetah@gmail.com>
2021-03-02 17:55:35 +0100

    Ex-4 done

--- ex-4/exercise.js
+++ ex-4/exercise.js
@@ -1,3 +1,15 @@
 function removecolor() {
+    let select = document.getElementById("colorSelect");
+    let option = select.getElementsByTagName("option")[0];
+    let remove = select.removeChild(option)
+    /*
+    let p = document.getElementsByTagName("p").innerHTML = remove;
+    window.alert(`${p} is removed`);
+    */
+    console.log(remove)
+   
+
+
+
 
 }
