2944044dbddaf46997bd6c78a3d17bad152e7820
fithawi ghide <lgfetah@gmail.com>
2021-03-02 17:53:24 +0100

    Ex-2 done

--- ex-2/README.md
+++ ex-2/README.md
@@ -2,6 +2,6 @@
 
 Write a JavaScript function to get the value of the href, hreflang, rel, target, and type attributes of the specified link.
 
-
 Print all the valueas like this:
+
 - 'The value of the href attribute of the link is : -value-'
--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -1,3 +1,25 @@
 function getAttributes() {
+   
+   
+   var id_value = document.getElementsByTagName("A")[0].getAttribute("id");
+   document.getElementsByTagName("p")[1].innerHTML = `The value of the id attribute is: ${id_value} ` ;
 
+   var type_value = document.getElementsByTagName("A")[0].getAttribute("type");
+   document.getElementsByTagName("p")[2].innerHTML = `The value of the type attribute is : ${ type_value}`;
+
+   var hreflang_value = document.getElementsByTagName("A")[0].getAttribute("hreflang");
+   document.getElementsByTagName("p")[3].innerHTML = `The value of the hreflang attribute is: ${ hreflang_value} `;
+
+   var rel_value = document.getElementsByTagName("A")[0].getAttribute("rel");
+   document.getElementsByTagName("p")[4].innerHTML = `The value of the rel attribute is: ${ rel_value}`;
+
+   
+   var target_value = document.getElementsByTagName("A")[0].getAttribute("target");
+   document.getElementsByTagName("p")[5].innerHTML = `The value of the target attribute is: ${target_value}`;
+
+
+   var href_value = document.getElementsByTagName("A")[0].getAttribute("href");
+   document.getElementsByTagName("p")[6].innerHTML = `The value of the href attribute of the link: ${href_value}`;
+
+   
 }
