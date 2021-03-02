a28e5c6e4c8fcf7d1531c3764bfb1284dff0f68b
fithawi ghide <lgfetah@gmail.com>
2021-03-02 17:52:33 +0100

    Ex-1 done

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,5 +1,12 @@
 // Use this function that is linked to the button to change the background
 
 function set_background() {
+    let p1 = document.getElementById("p1");
+    let p2 = document.getElementById("p2");
+    let p3 = document.getElementById("p3");
+    
+    p1.style.background = "red";
+    p2.style.background = "green";
+    p3.style.background = "yellow"
 
 }
