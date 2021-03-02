6f85da1efd4bc6e17ffbcec31f8124757f74e123
fithawi ghide <lgfetah@gmail.com>
2021-03-02 17:53:57 +0100

    Ex-3 done

--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -1,3 +1,14 @@
 function insert_Row() {
+      var table = document.getElementById("sampleTable");
+      var row = table.insertRow(2);
+      
+
+      var cell1 = row.insertCell(0);
+      var cell2 = row.insertCell(1);
+      console.log(cell1)
+      console.log(cell2)
+      
+     //cell1.innerHTML = "New Cell1";
+      //cell2.innerHTML = "New Cell2";
 
 }
