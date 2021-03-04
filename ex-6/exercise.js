345f1774e37360c4b794caa04226ac1958d0a18e
fithawi ghide <lgfetah@gmail.com>
2021-03-04 09:03:33 +0100

    Ex-4 done

--- ex-6/exercise.js
+++ ex-6/exercise.js
@@ -1,9 +1,14 @@
 function volume_sphere() {
 
   // This is how you calculate the volume of the sphere
-  radius = Math.abs(Number(radius));
-  volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+  var volume;
+  var radius = document.getElementById('radius').value;
+  radius = Math.abs(radius);
+  volume = (4/3) * Math.PI * Math.pow(radius, 3);
   volume = volume.toFixed(4);
+  document.getElementById('volume').value = volume;
+ 
+ } 
+window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
 
-}
 
